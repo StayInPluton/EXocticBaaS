b5afbe652c6468bb4faef0a26c0278f981d6b977
Pedro Heleno <111394219+StayInPluton@users.noreply.github.com>
2023-06-14 18:43:00 -0300

    alteração na api addUser.js para ter uma conexão com o banco de dados

--- EXocticBaaS/addUser.js
+++ EXocticBaaS/addUser.js
@@ -1,44 +1,106 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize('exoctic', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+
+sequelize.authenticate()
+    .then(function () {
+        console.log("Conectado ao banco de dados com sucesso!");
+    })
+    .catch(function (erro) {
+        console.log("Erro ao conectar ao banco de dados: " + erro);
+    });
+
+
+// Definição do modelo
+const Usuario = sequelize.define('Usuario', {
+    id_usuario: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique:true,
+        allowNull:false
+    },
+    cpf: {
+        type: Sequelize.BIGINT,
+        unique:true,
+        allowNull:false
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numero_telefone: {
+        type: Sequelize.BIGINT,
+        allowNull:false
+    },
+    nascimento: {
+        type: Sequelize.DATE,
+        allowNull:false
+    }
+
+
+}, { freezeTableName: true });
+
 
 app.use(bodyParser.json());
 
+Usuario.sync({})
+  .then(() => {
+    console.log('Tabela Usuarios criada com sucesso');
+  })
+  .catch((error) => {
+    console.log('Erro ao criar tabela Usuarios:', error);
+  });
+
+
 // Rota para cadastrar um novo usuário
 app.post('/cadastro', (req, res) => {
-    const { nome, email, cpf, senha } = req.body;
+    const { id_usuario, nome, email, cpf, senha, numero_telefone, nascimento } = req.body;
 
-    // Verifica se todos os campos foram fornecidos
-    if (!nome || !email || !cpf || !senha) {
+
+    if (!nome || !email || !cpf || !senha || !numero_telefone || !nascimento) {
         return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
     }
 
-    // Verifica se o usuário já está cadastrado (pelo email ou CPF)
-    const usuarioData = fs.readFileSync('users.json');
-    const usuarios = JSON.parse(usuarioData).usuarios;
-    const usuarioExistente = usuarios.find(
-        (usuario) => usuario.email === email || usuario.cpf === cpf
-    );
-    if (usuarioExistente) {
-        return res.status(409).json({ error: 'Usuário já cadastrado.' });
-    }
 
-    // Cria um novo usuário
-    const novoUsuario = {
+    Usuario.create({
+        id_usuario,
         nome,
         email,
         cpf,
         senha,
-    };
-
-    // Salva o novo usuário no arquivo usuario.json
-    usuarios.push(novoUsuario);
-    fs.writeFileSync('users.json', JSON.stringify({ usuarios }));
-
-    res.json({ message: 'Usuário cadastrado com sucesso.' });
+        numero_telefone,
+        nascimento
+    })
+        .then(() => {
+            res.json({ message: 'Usuário cadastrado com sucesso.' });
+        })
+        .catch((error) => {
+            console.log('Erro ao cadastrar o usuário:', error);
+            res.status(500).json({ error: 'Erro ao cadastrar o usuário.' });
+        });
 });
+
 
 app.listen(3000, () => {
     console.log('Servidor está ouvindo na porta 3000');
 });
+
+
+
